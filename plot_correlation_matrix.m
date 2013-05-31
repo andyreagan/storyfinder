49576bae1add955ccedea3383b398b317a233980
Andy Reagan <github@andyreagan.com>
2013-05-31 09:40:45 -0400

    plotted timeseries and corr matrix, included in paper

--- plot_correlation_matrix.m
+++ plot_correlation_matrix.m
@@ -5,6 +5,8 @@
 % written by Andy Reagan
 % 4/16/13
 
+clear all;
+
 % load the files
 valence_h = {'000','025','050','075','100','125','150','175','200'};
 
@@ -21,10 +23,47 @@ end
 % inds=find(pvalMat>.01);
 % corrMat(inds)=0; 
 
+tmp = size(corrMat);
+
+newCorrMat = ones(tmp(1)+1);
+
+newCorrMat(1:9,1:9) = corrMat;
+
 figure;
-pcolor(corrMat);
-colorbar;
-shading interp
+
+shadeB = 1; % if 0, flat squares (if 1, shade)
+if shadeB
+    pcolor(corrMat);
+    colorbar;
+    shading interp
+    tmpfilenoname = 'correlationMatrix_shade';
+else
+    pcolor(newCorrMat);
+    colorbar;
+    tmpfilenoname = 'correlationMatrix';
+end
+
+tmpylab=ylabel('$\Delta h$','fontsize',15,'verticalalignment','bottom','fontname','helvetica','interpreter','latex');
+set(tmpylab,'position',get(tmpylab,'position') + [-.35 0 0]);
+
+tmpxlab=xlabel('$\Delta h$','fontsize',15,'verticalalignment','bottom','fontname','helvetica','interpreter','latex');
+set(tmpxlab,'position',get(tmpxlab,'position') + [0 -.35 0]);
+
+valence_h = {'0','0.25','0.5','0.75','1','1.25','1.5','1.75','2'};
+
+if ~shadeB
+    set(gca,'xtick',1.5:1:9.5)
+    set(gca,'ytick',1.5:1:9.5)
+end
+
+set(gca,'xticklabel',valence_h)
+set(gca,'yticklabel',valence_h)
+
+psprintcpdf(tmpfilenoname);
+
+tmpcommand = sprintf('open %s.pdf;',tmpfilenoname); 
+system(tmpcommand);
+
 
 % figure;
 % clustergram(corrMat);
--- plot_timeseries.m
+++ plot_timeseries.m
@@ -11,7 +11,7 @@ tmpfigh = gcf;
 clf;
 %figshape(600,800);
 
-tmpfilename = 'presidents';
+tmpfilename = 'timeseries';
 
 tmpfilenoname = sprintf('%s_noname',tmpfilename);
 
@@ -33,7 +33,7 @@ set(gcf,'DefaultAxesLineWidth',0.5);
 set(gcf,'PaperPositionMode','auto');
 
 tmpsym = {'o','s','v','o','s','v'};
-tmpcol = {'g','b','r','k','c','m'};
+tmpcol = {[255 0 0]/255,[223 97 1]/255,[255 187 1]/255,[255 229 1]/255,'k',[125 255 255]/255,[75 202 255]/255,[71 71 255]/255,[0 0 255]/255};
 
 
 positions(1).box = [.2 .2 .7 .7]; % [ xmin ymin xmax ymax
@@ -56,27 +56,61 @@ for i=1:length(valence_h)
     %title(tmp);
     %ylim([5 7]);
     
-    line = plot(1:length(data(:,1)),data(:,1),'Color',tmpcol{mod(i,6)+1},'LineWidth',2);
+    tmph(i) = plot(1:length(data(:,1)),data(:,1),'Color',tmpcol{i},'LineWidth',1.4);
     hold on
     
     %tmph(i) = plot(1:length(data(1:3:end,1)),data(1:3:end,1),'Marker',tmpsym{mod(i,6)+1},'MarkerFaceColor',tmpcol{mod(i,6)+1},'LineStyle','none');
 end
 
+tmpylab=ylabel('Happs','fontsize',10,'verticalalignment','bottom','fontname','helvetica','interpreter','latex');
+set(tmpylab,'position',get(tmpylab,'position') + [-.05 0 0]);
 
-tmplegend = legend(tmph,valence_h,'location','northeast'); % {6},tmpleg{3},tmpleg{5},tmpleg{1},tmpleg{2},tmpleg{4}
-set(tmplegend,'position',get(tmplegend,'position')-[.02 0.02 0 0]); %% -[x y 0 0]
+tmpxlab=xlabel('Time','fontsize',10,'verticalalignment','bottom','fontname','helvetica','interpreter','latex');
+set(tmpxlab,'position',get(tmpxlab,'position') + [0 -.05 0]);
+
+% valence_h_dec=zeros(1,length(valence_h));
+% for j=1:length(valence_h)   
+%     valence_h_dec(j) = num2str(str2num(valence_h{j})/100);
+% end
+
+valence_h = {'0.00','0.25','0.50','0.75','1.00','1.25','1.50','1.75','2.00'};
+
+
+% Legend at axes 1
+tmplegend = legend(gca,tmph(1:4),valence_h{1:4},1); % {6},tmpleg{3},tmpleg{5},tmpleg{1},tmpleg{2},tmpleg{4}
+set(tmplegend,'position',get(tmplegend,'position')-[.11 0 0 0]); %% -[x y 0 0]
 %% change font
 tmplegend = findobj(tmplegend,'type','text');
-set(tmplegend,'FontSize',16);
+set(tmplegend,'FontSize',9);
 %% remove box:
 legend boxoff
 
+% Block 2
+% Axes handle 2 (unvisible, only for place the second legend)
+ah2=axes('position',get(gca,'position'), 'visible','off');
+% Legend at axes 2
+tmplegend = legend(ah2,tmph(5:9),valence_h{5:9},1); % {6},tmpleg{3},tmpleg{5},tmpleg{1},tmpleg{2},tmpleg{4}
+set(tmplegend,'position',get(tmplegend,'position')-[0 0 0 0]); %% -[x y 0 0]
+% %% change font
+tmplegend = findobj(tmplegend,'type','text');
+set(tmplegend,'FontSize',9);
+% %% remove box:
+legend boxoff
+% 
+% % Block 3
+% % Axes handle 3 (unvisible, only for place the third legend)
+% ah3=axes('position',get(gca,'position'), 'visible','off');
+% % Legend at axes 3
+% legend(ah3,h(5:6),'y5','y6',3)
 
-%psprintcpdf(tmpfilenoname);
 
 
-%tmpcommand = sprintf('open %s.pdf;',tmpfilenoname); 
-%system(tmpcommand);
+
+psprintcpdf(tmpfilenoname);
+
+
+tmpcommand = sprintf('open %s.pdf;',tmpfilenoname); 
+system(tmpcommand);
 
 % system('epstopdf distribution-comparison001_noname.ps','-echo');
 % system('open distribution-comparison001_noname.pdf');
